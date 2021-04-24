8c0c29498b1094bb49090143a1c0b71117b35bb5
Sjon de Gast <sjondegast@gmail.com>
2021-04-24 17:29:07 +0200

    removed extra blank line end of file

--- rptree/rptree.py
+++ rptree/rptree.py
@@ -24,3 +24,18 @@ class DirectoryTree:
         tree = self._generator.build_tree()
         for entry in tree:
             print(entry)
+
+
+class _TreeGenerator:
+    def __init__(self, root_dir):
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = []
+
+    def build_tree(self):
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
