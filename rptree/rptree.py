0b149d856f7eecce8de414f3c9aa463b46130cdc
Sjon de Gast <sjondegast@gmail.com>
2021-04-26 00:36:59 +0200

    added _tree_body() definition and def _add_directory and file to create the body of the tree

--- rptree/rptree.py
+++ rptree/rptree.py
@@ -40,3 +40,31 @@ class _TreeGenerator:
         self._tree.append(f"{self._root_dir}{os.sep}")
         self._tree.append(PIPE)
 
+    def _tree_body(self, directory, prefix=""):
+        entrees = directory.iterdir()
+        entries = sorted(entrees, key=lambda entry: entry.is_file())
+        entries_count = len(entries)
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(
+                    entry, index, entries_count, prefix, connector
+                )
+            else:
+                self._add_file(entry, prefix, connector)
+
+    def _add_directory(
+            self, directory, index, entries_count, prefix, connector
+    ):
+        self._tree.append(f"{prefix}{connector} {directory.name}{os.sep}")
+        if index != entries_count - 1:
+            prefix += PIPE_PREFIX
+        else:
+            prefix += SPACE_PREFIX
+        self._tree_body(
+            directory=directory,
+            prefix=prefix,
+        )
+
+    def _add_file(self, file, prefix, connector):
+        self._tree.append(f"{prefix}{connector} {file.name}")
