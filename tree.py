c57718f1ed8a1c7c19a6ef64948218a30307db91
Sjon de Gast <sjondegast@gmail.com>
2021-04-26 18:44:08 +0200

    added entry point file imported the cli and the main function and used if to initialize the main function

--- tree.py
+++ tree.py
@@ -1,2 +1,11 @@
-# entry-point script for you to run the application
+#!/usr/bin/env python3
+# tree.py
+
+"""This module provides RP Tree entry point script."""
+
+from rptree.cli import main
+
+
+if __name__ == "__main__":
+    main()
 
