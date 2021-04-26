08d6427834985d481ee4fea3cf1a520e18619cae
Sjon de Gast <sjondegast@gmail.com>
2021-04-26 18:14:01 +0200

    build basic main function, next: build the parse_cmd_line_arguments() function for cli-related features

--- rptree/cli.py
+++ rptree/cli.py
@@ -1,3 +1,25 @@
-# command-line interface for the application
-# CLI-related code, provides the CLI
+"""This module provides the RP Tree CLI."""
+# cli.py
 
+import argparse
+import pathlib
+import sys
+
+
+from . import __version__
+from .rptree import DirectoryTree
+
+
+def main():
+    # TODO: build parse_cmd_line_arguments() function
+    args = parse_cmd_line_arguments()
+    # turn root_dir into a pathlib object
+    root_dir = pathlib.Path(args.root_dir)
+    # TODO: if not root_dir.is_dir(): how to avoid sys.exit() or why do we need to do sys.exit()?
+    # check if root_dir is a valid directory
+    if not root_dir.is_dir():
+        print("The specified root directory doesn't exist")
+        sys.exit()
+    # TODO: build tree object and generate tree with .generate()
+    tree = DirectoryTree(root_dir)
+    tree.generate()
